f2e8578bfa1d731c033e0bb8cabc7be5da0f4b93
yuvraj1905 <yuvrajkumar2509@gmail.com>
2022-08-17 01:31:18 +0530

    Added api

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'a9f7ae41-970b-4015-b571-714c3c906104';
 export const MODAL_CLOSE_SEC = 2.5;
